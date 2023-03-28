b6b03270ead6a8f6bd975a64bca1e2a1400d0b23
andreacoppari <andrea.coppari@proton.me>
2023-03-28 16:31:54 +0200

    baseline

---
+++ clip.py
@@ -0,0 +1,18 @@
+import torch
+from CLIP import clip
+from PIL import Image
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+image = preprocess(Image.open("woof_meow.jpg")).unsqueeze(0).to(device)
+text = clip.tokenize(["a dog and a cat", "a dog", "a cat", "right cat", "left dog"]).to(device)
+
+with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+    
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
---
+++ yoloclip.py
@@ -0,0 +1,54 @@
+import torch
+from CLIP import clip
+from PIL import Image, ImageDraw
+import cv2
+
+### Detect BBoxes
+
+# Models
+yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# Images
+img = 'CLIP/CLIP.png'
+
+# Query
+q = "A pink diagram"
+
+# Inference
+results = yolo_model(img)
+
+# Results
+results.print()
+results.show()
+#      xmin    ymin    xmax   ymax  confidence  class    name
+
+
+### Choose best BBox
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+text = clip.tokenize(q).to(device)
+
+max_prob = 0
+for bbox in results.xyxy[0].cpu().numpy():
+    image = cv2.imread(img)
+    image = image[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+    cv2.imwrite("crop.png", image)
+    image = preprocess(Image.fromarray(image)).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        image_features = model.encode_image(image)
+        text_features = model.encode_text(text)
+        
+        logits_per_image, logits_per_text = model(image, text)
+        prob = logits_per_text.cpu().numpy()[0][0]
+        print(prob)
+        if prob > max_prob:
+            max_prob = prob
+            best_bbox = bbox
+
+source_img = Image.open(img).convert("RGBA")
+draw = ImageDraw.Draw(source_img)
+draw.rectangle(((best_bbox[0], best_bbox[1]), (best_bbox[2], best_bbox[3])))
+source_img.save("test.png", "PNG")
