3c03c402dd6d63360a99a261be9f40c8f3add3ed
andreacoppari <andrea.coppari@proton.me>
2023-03-31 21:28:44 +0200

    upgraded baseline

--- yoloclip.py
+++ yoloclip.py
@@ -2,6 +2,7 @@ import torch
 from CLIP import clip
 from PIL import Image, ImageDraw
 import cv2
+import numpy as np
 
 ### Detect BBoxes
 
@@ -9,16 +10,16 @@ import cv2
 yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
 # Images
-img = 'woof_meow.jpg'
+img = 'zidane.jpg'
 
 # Query
-q = "A photo of a cat"
+q = "A photo of a little tie"
 
 # Inference
 results = yolo_model(img)
 
 # Results
-results.print()
+# results.print()
 results.show()
 #      xmin    ymin    xmax   ymax  confidence  class    name
 
@@ -32,9 +33,9 @@ text = clip.tokenize(q).to(device)
 
 max_prob = 0
 for bbox in results.xyxy[0].cpu().numpy():
-    image = cv2.imread(img)
-    image = image[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
-    cv2.imwrite("crop.png", image)
+    temp = cv2.imread(img)
+    image = np.zeros((temp.shape[0], temp.shape[1], temp.shape[2]), dtype=np.uint8)
+    image[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])] = temp[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
     image = preprocess(Image.fromarray(image)).unsqueeze(0).to(device)
 
     with torch.no_grad():
@@ -43,12 +44,12 @@ for bbox in results.xyxy[0].cpu().numpy():
         
         logits_per_image, logits_per_text = model(image, text)
         prob = logits_per_text.cpu().numpy()[0][0]
-        print(prob)
+
         if prob > max_prob:
             max_prob = prob
             best_bbox = bbox
 
 source_img = Image.open(img).convert("RGBA")
 draw = ImageDraw.Draw(source_img)
-draw.rectangle(((best_bbox[0], best_bbox[1]), (best_bbox[2], best_bbox[3])))
+draw.rectangle(((best_bbox[0], best_bbox[1]), (best_bbox[2], best_bbox[3])), width=3)
 source_img.save("test.png", "PNG")
